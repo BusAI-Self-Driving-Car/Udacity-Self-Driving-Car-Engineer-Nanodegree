c88b5d79f1abc03a93b081a1cfbd3849dbed1c77
wetoo-cando <31558837+wetoo-cando@users.noreply.github.com>
2017-10-19 11:49:19 +0530

    Term1-proj2: adapted CNN arch. and training pipeline from thre previous LeNet-lab for this project

--- Term1-proj2-Traffic-Sign-Classifier/Traffic_Sign_Classifier.py
+++ Term1-proj2-Traffic-Sign-Classifier/Traffic_Sign_Classifier.py
@@ -110,9 +110,17 @@ print("Number of classes =", n_classes)
 
 ### Data exploration visualization code goes here.
 ### Feel free to use as many code cells as needed.
+import random
 import matplotlib.pyplot as plt
 # Visualizations will be shown in the notebook.
-get_ipython().run_line_magic('matplotlib', 'inline')
+#get_ipython().run_line_magic('matplotlib', 'inline')
+
+index = random.randint(0, len(X_train))
+image = X_train[index].squeeze()
+
+#plt.figure(figsize=(1,1))
+#plt.imshow(image, cmap="gray")
+print(y_train[index])
 
 
 # ----
@@ -148,6 +156,25 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 ### Preprocess the data here. It is required to normalize the data. Other preprocessing steps could include 
 ### converting to grayscale, etc.
 ### Feel free to use as many code cells as needed.
+from sklearn.utils import shuffle
+
+def preprocess_images(X_in):
+
+    X = np.float32(X_in)
+
+    # standardize features
+    X -= 128.
+    X /= 128.
+
+    return X
+
+X_train = preprocess_images(X_train)
+X_test = preprocess_images(X_test)
+X_valid = preprocess_images(X_valid)
+
+X_train, y_train = shuffle(X_train, y_train)
+X_test, y_test = shuffle(X_test, y_test)
+X_valid, y_valid = shuffle(X_valid, y_valid)
 
 
 # ### Model Architecture
@@ -165,6 +192,98 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 # sets imply underfitting. A high accuracy on the training set but low accuracy on the validation set implies overfitting.
 
 # In[1]:
+from tensorflow.contrib.layers import flatten
+
+def conv2d(x, W, b, strides=1):
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='VALID')
+    x = tf.nn.bias_add(x, b)
+    return x
+
+def activation_relu(x):
+    return tf.nn.relu(x)
+
+def activation_sigmoid(x):
+    return tf.nn.sigmoid(x)
+
+def maxpool2d(x, k=2):
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='VALID')
+
+def full_connection(x, W, b):
+    return tf.matmul(x, W) + b
+
+def LeNet(x):    
+    # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
+    mu = 0
+    sigma = 0.1
+
+    weights = {
+        'w_conv_1': tf.Variable(tf.truncated_normal((5, 5, image_shape[3], 6), mean=mu, stddev=sigma)),
+        'w_conv_2': tf.Variable(tf.truncated_normal((5, 5, 6, 16), mean=mu, stddev=sigma)),
+        'w_fc_1': tf.Variable(tf.truncated_normal((400, 120), mean=mu, stddev=sigma)),
+        'w_fc_2': tf.Variable(tf.truncated_normal((120, 84), mean=mu, stddev=sigma)),
+        'w_fc_3': tf.Variable(tf.truncated_normal((84, n_classes), mean=mu, stddev=sigma))
+    }
+
+    biases = {
+        'b_conv_1': tf.Variable(tf.zeros(6)),
+        'b_conv_2': tf.Variable(tf.zeros(16)),
+        'b_fc_1': tf.Variable(tf.zeros(120)),
+        'b_fc_2': tf.Variable(tf.zeros(84)),
+        'b_fc_3': tf.Variable(tf.zeros(n_classes))
+    }
+    
+    # TODO: Layer 1: Convolutional. Input = image_shape. Output = 28x28x6.
+    conv1 = conv2d(x, weights['w_conv_1'], biases['b_conv_1'], strides=1)
+    print("conv1.shape = {}".format(conv1.get_shape()))
+
+    # TODO: Activation.
+    #act1 = activation_relu(conv1)
+    act1 = activation_relu(conv1)
+    print("act1.shape = {}".format(act1.get_shape()))
+
+    # TODO: Pooling. Input = 28x28x6. Output = 14x14x6.
+    pool1 = maxpool2d(act1, k=2)
+    print("pool1.shape = {}".format(pool1.get_shape()))
+
+    # TODO: Layer 2: Convolutional. Output = 10x10x16.
+    conv2 = conv2d(pool1, weights['w_conv_2'], biases['b_conv_2'], strides=1)
+    print("conv2.shape = {}".format(conv2.get_shape()))
+    
+    # TODO: Activation.
+    act2 = activation_relu(conv2)
+    print("act2.shape = {}".format(act2.get_shape()))
+
+    # TODO: Pooling. Input = 10x10x16. Output = 5x5x16.
+    pool2 = maxpool2d(act2, k=2)
+    print("pool2.shape = {}".format(pool2.get_shape()))
+
+    # TODO: Flatten. Input = 5x5x16. Output = 400.
+    x_flat = flatten(pool2)
+    print("x_flat.shape = {}".format(x_flat.get_shape()))
+
+    # TODO: Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1 = full_connection(x_flat, weights['w_fc_1'], biases['b_fc_1'])
+    print("fc1.shape = {}".format(fc1.get_shape()))
+    
+    # TODO: Activation.
+    #act2 = activation_relu(fc1)
+    act2 = activation_sigmoid(fc1)
+    print("act2.shape = {}".format(act2.get_shape()))
+
+    # TODO: Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2 = full_connection(act2, weights['w_fc_2'], biases['b_fc_2'])
+    print("fc2.shape = {}".format(fc2.get_shape()))
+    
+    # TODO: Activation.
+    #act3 = activation_relu(fc2)
+    act3 = activation_sigmoid(fc2)
+    print("act3.shape = {}".format(act3.get_shape()))
+
+    # TODO: Layer 5: Fully Connected. Input = 84. Output = n_classes.
+    logits = full_connection(act3, weights['w_fc_3'], biases['b_fc_3'])
+    print("logits.shape = {}".format(logits.get_shape()))
+    
+    return logits
 
 
 ### Train your model here.
@@ -172,6 +291,78 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 ### Once a final model architecture is selected, 
 ### the accuracy on the test set should be calculated and reported as well.
 ### Feel free to use as many code cells as needed.
+
+# Set up tensorflow variables
+x = tf.placeholder(tf.float32, (None, 32, 32, image_shape[3]))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, n_classes)
+
+# Training Pipeline
+rate = 0.001
+
+logits = LeNet(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
+
+# Model Evaluation
+# Evaluate how well the loss and accuracy of the model for a given dataset.
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+# ## Train the Model
+# Run the training data through the training pipeline to train the model.
+# 
+# Before each epoch, shuffle the training set.
+# 
+# After each epoch, measure the loss and accuracy of the validation set.
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+    
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+            
+        validation_accuracy = evaluate(X_validation, y_validation)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+        
+    saver.save(sess, './lenet')
+    print("Model saved")
+
+# ## Evaluate the Model
+# Once you are completely satisfied with your model, evaluate the performance of the model on the test set.
+# 
+# Be sure to only do this once!
+# 
+# If you were to measure the performance of your trained model on the test set, then improve your model, and then measure the performance of your model on the test set again, that would invalidate your test results. You wouldn't get a true measure of how well your model would perform against real data.
+# 
+# You do not need to modify this section.
+with tf.Session() as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('.'))
+
+    test_accuracy = evaluate(X_test, y_test)
+    print("Test Accuracy = {:.3f}".format(test_accuracy))
+
 
 
 # ---
