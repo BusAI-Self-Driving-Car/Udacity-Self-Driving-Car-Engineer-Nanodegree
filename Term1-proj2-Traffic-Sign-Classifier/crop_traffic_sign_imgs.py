309dc96323f8ad8ef5b779291320cdb07bd145ab
wetoo-cando <31558837+wetoo-cando@users.noreply.github.com>
2017-11-06 20:02:49 +0100

    Removing unnecessary files

--- Term1-proj2-Traffic-Sign-Classifier/crop_traffic_sign_imgs.py
+++
@@ -1,9 +0,0 @@
-import os
-import cv2
-
-input_imgs = os.listdir(".")
-
-for image in input_imgs: 
-	img = cv2.imread(image, cv2.IMREAD_COLOR)
-	resized_img = cv2.resize(img, (32, 32))
-	cv2.imwrite("cropped/"+image, resized_img)
