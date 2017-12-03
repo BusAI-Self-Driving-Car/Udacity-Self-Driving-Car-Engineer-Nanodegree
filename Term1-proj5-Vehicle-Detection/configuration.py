efdb39396be62816be01ea1d228e9688ced2dd09
wetoo-cando <31558837+wetoo-cando@users.noreply.github.com>
2017-12-03 11:03:32 +0100

    Switch to YCrCb colorspace for HOG as well as color feature extraction

--- Term1-proj5-Vehicle-Detection/configuration.py
+++ Term1-proj5-Vehicle-Detection/configuration.py
@@ -14,20 +14,23 @@ dict_config_params = {
     'use_spatial_feat':True, 
     'use_hist_feat':True,     
     
-    # Common params for color and HOG features
-    #'color_space':'RGB',
-    
     # Color feature params
-    'color_cspace':'RGB',
+    'color_cspace':'YCrCb',
     'color_spatial_size':(32, 32),
     'color_hist_bins':32,
     'color_hist_range':(0, 256),
     
     # HOG feature params
-    'use_gray_img':True,
+    'use_gray_img':False,
     'hog_channel':'ALL', #'ALL',
-    'hog_cspace':'RGB',
+    'hog_cspace':'YCrCb',
     'hog_n_orientations': 9,
     'hog_pixels_per_cell': 8,
-    'hog_cells_per_block': 2
+    'hog_cells_per_block': 2,
+    'hog_subsampling_max': 3,
+    'hog_subsampling_step': 1,
+    
+    # Heatmap
+    'heat_threshold':2,
+    'buffer_len_hotwindows':7,
 }
