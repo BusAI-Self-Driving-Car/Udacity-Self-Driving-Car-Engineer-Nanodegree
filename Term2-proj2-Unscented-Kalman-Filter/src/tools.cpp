6a8f39dce0d059f338707115e4fb0b00e41ca415
Rahul Chaudhari <31558837+wetoo-cando@users.noreply.github.com>
2018-02-03 02:17:19 +0100

    Calculate RMSE

--- Term2-proj2-Unscented-Kalman-Filter/src/tools.cpp
+++ Term2-proj2-Unscented-Kalman-Filter/src/tools.cpp
@@ -15,4 +15,27 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+    VectorXd diff(4), rmse(4);
+    diff << 0,0,0,0;
+    rmse << 0,0,0,0;
+
+    if (estimations.size() != ground_truth.size())
+    {
+        throw std::runtime_error ("Input vectors must have the same size!");
+    }
+    else if(estimations.size() == 0)
+    {
+        throw std::runtime_error ("Input vectors have zero length!");
+    }
+
+    for(std::size_t i = 0; i < estimations.size(); ++i)
+    {
+        diff = estimations[i] - ground_truth[i];
+        diff = diff.array() * diff.array();
+
+        rmse += diff;
+    }
+
+    rmse = rmse/estimations.size();
+    return rmse.array().sqrt();
 }
