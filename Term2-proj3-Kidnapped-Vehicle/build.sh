892198e8dc59727d8044a862c08d365fb4ef4bc7
Rahul Chaudhari <31558837+wetoo-cando@users.noreply.github.com>
2018-03-08 06:56:07 +0100

    Extend build script to use more recent cmake and make

--- Term2-proj3-Kidnapped-Vehicle/build.sh
+++ Term2-proj3-Kidnapped-Vehicle/build.sh
@@ -12,8 +12,35 @@
 # Go into the directory where this bash script is contained.
 cd `dirname $0`
 
+export PATH=~/Documents/udas/software/cmake-3.6.0-Linux-x86_64/bin:$PATH
+export PATH=~/Documents/udas/software/make-4.2:$PATH
+
+# Symlink to a newer GCC (required by this project)
+sudo ln -s -f /usr/bin/gcc-6 /usr/bin/gcc
+sudo ln -s -f /usr/bin/g++-6 /usr/bin/g++
+
+echo ""
+echo "CMAKE: $(which cmake)"
+echo "MAKE: $(which make)"
+echo ""
+echo "Created new GCC / G++ symlinks for gXX-6:"
+echo "GCC: $(ls -l $(which gcc))"
+echo "G++: $(ls -l $(which g++))"
+echo ""
+
 # Compile code.
 mkdir -p build
 cd build
 cmake ..
 make -j `nproc` $*
+
+# Revert original symlinks
+sudo ln -s -f /usr/bin/gcc-4.8 /usr/bin/gcc
+sudo ln -s -f /usr/bin/g++-4.8 /usr/bin/g++
+
+echo ""
+echo "Restoring original GCC / G++ symlinks ..."
+echo "GCC: $(ls -l $(which gcc))"
+echo "G++: $(ls -l $(which g++))"
+echo ""
+
